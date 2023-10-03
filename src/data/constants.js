41b351890801550fd279b1fde4d8000b91fba762
Arijeet <arijeetbanerjee118@gmail.com>
2023-10-03 19:12:40 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,412 @@
+export const Bio = {
+  name: "Arijeet Banerjee",
+  roles: [
+    "Software Engineer",
+    "Full Stack Developer",
+    "UI/UX Designer",
+    "Competitive Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning and a growth mindset, I am always eager to make any meaningful contribution.Furthermore, a key strength of mine is communication, building strong relationships with people in order to deliver the best results.",
+  github: "https://github.com/ArijeetBanerjee",
+  resume:
+    "https://drive.google.com/file/d/1o_Hi71ytLp7FA1bT1vk3DoUY1mLBM3uV/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/arijeet-banerjee/",
+  twitter: "https://twitter.com/ARIJEET118",
+  insta: "https://www.instagram.com/a.r.i.j.e.e.t_20/",
+  facebook: "https://www.facebook.com/arijeet.banerjee.9250/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+     
+     
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      
+      
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR49CPLVKPAe1cslXaHnF20_Qwt-MiSoRen-vvYNoCmLkKwCQ1GtWdstwiUeDJ03RYu5ik&usqp=CAU",
+      },
+      
+      {
+        name: "C++",
+        image:
+          "https://i.redd.it/31b2ii8hchi31.jpg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://i0.wp.com/www.elearningworld.org/wp-content/uploads/2019/04/MySQL.svg.png?fit=600%2C400&ssl=1",
+      },
+     
+      {
+        name: "MongoDB",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXZPIWevT2KaAZdQ8UqoEHMPAxBRdgwOn9nhk_0PJNLx3Yfwcf17MnE1vKlHa_nXLNWmE&usqp=CAU",
+      },
+      
+    ],
+  },
+  {
+    title: "Android",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "Kotlin",
+        image:
+          "https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg",
+      },
+      {
+        name: "XML",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBMw6_RdwKQ9bDFfnKDX1iwMl4bVJEvd9PP53XuIw&s",
+      },
+      {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/D4D0BAQFTpfRDuChAOQ/company-logo_200_200/0/1680453080332?e=2147483647&v=beta&t=0PWAAQjDvmFBOhKvd2WJA58DjTtx2yojkT7Jxj7-R9w",
+    role: "Software Developer",
+    company: "Prodigy Infotech",
+    date: "Oct 2023",
+    desc: "1) Developed a user-friendly image processing tool encompassing features like image loading, display, basic filters, color adjustments, cropping, resizing, and image saving, demonstrating proficiency in software development and user interface design.",
+    skills: [ 
+      " C++",
+    ],
+    doc: "https://media.licdn.com/dms/image/D4D22AQGwPDc2QAmllA/feedshare-shrink_1280/0/1695571109013?e=1698883200&v=beta&t=oxiDMI-TantOwej2oOMRrxcEVNg_6eVL-2gaAuxHPpY",
+  },
+  {
+    id: 0,
+    img: "https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=608,fit=crop,q=95/Aq20eV79zLfpXV6b/logo-png-mnl7npnlXjHPl9KV.png",
+    role: "C++ Programming intern",
+    company: "Codsoft",
+    date: "Oct 2023",
+    desc: "Developed a robust web scraping program to extract critical product details, including names, prices, and ratings, from various online e-commerce websites and saving the data in a structured CSV format, demonstrating expertise in data retrieval and storage for analytical purposes.",
+    skills: [
+      "C++",
+    ],
+    doc: "https://media.licdn.com/dms/image/D4D22AQEsnjL0odTnVA/feedshare-shrink_1280/0/1695570870312?e=1698883200&v=beta&t=oN-FWgOEOPnxGeNc4veRU1jD4rOZg7X3EvZcpTDFlJY",
+  },
+  {
+    id: 1,
+    img: "https://octanet.in/storage/2023/04/cropped-ORANGE-WHITE-002.png",
+    role: "Web Development Intern",
+    company: "OctaNe SW. Services",
+    date: "Oct 2023",
+    desc: "Built an QR Code Generator Web App where QR of any website is generated just by entering the website link.",
+    skills: [
+      "ReactJS",
+      "ExpressJs",
+      "NodeJs",
+      "Bootstrap",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Ejs",
+      "MongoDB",
+    ],
+    doc:"https://media.licdn.com/dms/image/D4D22AQESdEwMEV3J_g/feedshare-shrink_1280/0/1694900390790?e=1698883200&v=beta&t=9uzo_xITWwZ1r7WXkJ5PkVIqAsDgW0PYCTl8dnUoSX4"
+  },
+  {
+    id: 2,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/girlScript.jpeg?alt=media&token=e656a621-cf3c-4230-bf0f-e74b4cec6035",
+    role: "Open Source Contributor ",
+    company: "GirlScript Summer of Code",
+    date: "May 2023 - Present",
+    desc: "Contributing to different open-source projects and learn from industry experts",
+    doc: "https://media.licdn.com/dms/image/D4D22AQEH-mgG3nw8ow/feedshare-shrink_1280/0/1684045710617?e=1698883200&v=beta&t=ixWoa4mpvH9_tS_xflHHL0-uP5QHpxeXZH1uxYaVGic",
+  },
+  {
+    id: 3,
+    img: "https://media.licdn.com/dms/image/D4E03AQETIDrERRD4UA/profile-displayphoto-shrink_800_800/0/1679878435769?e=2147483647&v=beta&t=-yNPFnOFANHbqQ_GszjM2oFu2bkJ4k49vA5Up0ovTOI",
+    role: "Microsoft Learn Student Ambassador",
+    company: "Microsoft",
+    date: "Nov2021 - Present",
+    desc: "During my tenure as a Microsoft Learn Student Ambassador, I enthusiastically hosted C++ events, conducted technical workshops, and collaborated on diverse tech projects. I engaged with my local community, mentored aspiring students, created educational content, and explored Microsoft technologies like Azure. ",
+    skills: [
+      "Leadership",
+      "Azure",
+      "Power BI",
+      "Communication",
+      "Networking",
+    ],
+    doc: "https://media.licdn.com/dms/image/D4D22AQF8RTQsfy8dOw/feedshare-shrink_800/0/1686661536895?e=1698883200&v=beta&t=NvNOGmZFrC_HDavxwPiJqyeTMO3fKrOviRVCR8Koe4c",
+  },
+  
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKPhz99Hi8WHiTP4ZTncFjG_hdTwW6CYBApdKC4RUNJwBLmOd6JnFm7Fk4VW4v-jTqxy4&usqp=CAU",
+    school: "Techno India University,Kolkata",
+    date: "Oct 2021 - Sep 2025",
+    grade: "8.71 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Techno India University, Kolkata. I have completed 7 semesters and have a CGPA of 8.59. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks. I am also a member of the Google Developers Student Club (GDSC) at TIU, where I am learning and working on exciting projects with a team of talented developers.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2jfH-faEmgrBPvl8Bbp89XKiTT4azcki7k43tev_0QVFo8D5BNcfCcTG4ryavp2T4qQ&usqp=CAU",
+    school: "DAV MODEL SCHOOL,Durgapur",
+    date: "Apr 2019 - Apr 2020",
+    grade: "85%",
+    desc: "I completed my class 12 high school education at  DAV Model School, Durgapur, where I studied Science.",
+    degree: "CBSE(XII), Science",
+  },
+  {
+    id: 2,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2jfH-faEmgrBPvl8Bbp89XKiTT4azcki7k43tev_0QVFo8D5BNcfCcTG4ryavp2T4qQ&usqp=CAU",
+    school: "DAV MODEL SCHOOL,Durgapur",
+    date: "Apr 2017 - Apr 2018",
+    grade: "88.2%",
+    desc: "I completed my class 10 education at DAV Model School, Durgapur",
+    degree: "CBSE(X)",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "HOTEL MANAGEMENT SYSTEM",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "About System, This Hotel Management system project is designed in ‘C++’ language. This system is based on a concept to store and generate all the records of the hotel and its booking. Also, this program is considered as a simple database of hotel room booking where the user can store bookings’ records easily as it is not time-consuming. Moreover, the user can add the room’s data, view a list of rooms, modify, and remove booking details.",
+    image:
+      "https://user-images.githubusercontent.com/76873956/237977609-02951c3c-3918-4e96-93e4-b3f1774284c4.png",
+    tags: [
+      "C++",
+    ],
+    category: "Backend",
+    github: "https://github.com/ArijeetBanerjee/HOTEL-MANAGEMENT-SYSTEM",
+    webapp: "https://github.com/ArijeetBanerjee/HOTEL-MANAGEMENT-SYSTEM",
+  },
+  {
+    id: 0,
+    title: "Weather Update API",
+    date: "Apr 2023 - May 2023",
+    description:
+      " Developed a node js (express and HTTPS) backend-based web application to get live weather updates by entering the location/city details from the front end. To get the weather details I have used a web scrapping technique on a popular open-source external weather API.",
+    image:
+      "https://user-images.githubusercontent.com/76873956/237973345-d1b24be1-0bdc-4d44-9e2d-911aade3f343.jpg",
+    tags: ["HTML","CSS","JavaScript"],
+    category: "web app",
+    github: "https://github.com/ArijeetBanerjee/Weather-Update-API",
+    webapp: "https://arijeetbanerjee.github.io/Weather-Update-API/",
+  },
+  {
+    id: 1,
+    title: "SIMON GAME",
+    date: "Oct 2022 - Present",
+    description:
+      "Designed and developed a classic SIMON Game, implementing it with a frontend built using HTML and CSS, while incorporating interactivity and functionality through JavaScript. This project showcased my web development skills and demonstrated my ability to create engaging and interactive web-based games.",
+    image:
+      "https://user-images.githubusercontent.com/76873956/237994343-c9a792fd-6c8d-4d7f-ba2f-105bdedac592.png",
+    tags: [
+      "HTML",
+      "CSS",
+      "JavaScript"
+    ],
+    category: "web app",
+    github: "https://github.com/ArijeetBanerjee/SIMON-GAME",
+    webapp: "https://arijeetbanerjee.github.io/SIMON-GAME/",
+  },
+  {
+    id: 2,
+    title: "Image Processing Tool",
+    date: "Jan 2023 - Mar 2023",
+    description:
+      "This is a powerful image processing application developed in C++ using the Qt framework. It allows users to perform various image manipulation operations such as resizing, cropping, filtering, color adjustment, and more. The application comes with a user-friendly interface, making it easy for both beginners and advanced users to enhance their images.Features are ✨ Load Image: Seamlessly load images into the application. 🖼️ Display Image: Visualize the loaded image within the app for easy reference. 🌈 Image Filters: Apply basic filters, including grayscale, blur, and sharpen. 🎨 Color Adjustment: Fine-tune image colors, brightness, and contrast to perfection. ✂️ Crop and Resize: Precisely adjust image dimensions with our user-friendly tools. 💾 Image Saving: Save your edited masterpiece effortlessly.",
+    image:
+      "https://user-images.githubusercontent.com/76873956/272174325-01e7c105-96fd-471c-8553-b956983b5e2f.png",
+    tags: ["C++","QT"],
+    category: "Backend",
+    github: "https://github.com/ArijeetBanerjee/Image_Processing_APP",
+    webapp: "https://github.com/ArijeetBanerjee/Image_Processing_APP",
+  },
+  {
+    id: 3,
+    title: "E-Bike Web UI Design",
+    date: "Dec 2021 - Apr 2022",
+    description:
+      "Designed an E-bike web user interface (UI) using Figma, combining my creativity and UI/UX design skills to craft an intuitive and visually appealing digital experience for users. This project allowed me to explore Figma's design capabilities and apply them to a real-world application, enhancing my proficiency in UI design.",
+    image:
+      "https://user-images.githubusercontent.com/76873956/271308643-3d2f6e2b-998c-42a0-b8e5-168707e62a16.png",
+    tags: ["Figma"],
+    category: "UI/UX",
+    github: "https://github.com/ArijeetBanerjee/UI-UX-Design",
+    webapp: "https://github.com/ArijeetBanerjee/UI-UX-Design",
+  },
+  {
+    id: 10,
+    title: "Gaming Console UI Design",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "Crafted a captivating gaming console user interface (UI) design using Figma, leveraging my skills in UI/UX design to create an engaging and immersive digital environment for gamers. This project enabled me to delve into the world of gaming interface design, honing my abilities in Figma and enhancing my understanding of user-centered design principles in the context of gaming.",
+    image:
+      "https://user-images.githubusercontent.com/76873956/271311180-470d52ec-fca1-4f4c-b941-a6fde87ea91a.png",
+    tags: ["Figma"],
+    category: "UI/UX",
+    github: "https://github.com/ArijeetBanerjee/UI-UX-Design",
+    webapp: "https://github.com/ArijeetBanerjee/UI-UX-Design",
+  },
+  {
+    id: 4,
+    title: "DRUM KIT",
+    date: "Jul 2021",
+    description:
+      "Created a game called drum kit which gives users the option to play different intrumental sounds, they can start playing it with keyboard or mouse.",
+    image:
+      "https://user-images.githubusercontent.com/76873956/237994663-ab8e2fc5-09c2-4f64-af3c-7f8fd51d311c.png",
+    tags: ["HTML","CSS","JavaScript"],
+    category: "web app",
+    github: "https://github.com/ArijeetBanerjee/DRUM-KIT",
+    webapp: "https://arijeetbanerjee.github.io/DRUM-KIT/",
+  },
+  {
+    id: 5,
+    title: "DICE GAME",
+    date: "Jun 2021",
+    description:
+      " This is a basic multiplayer game with a random probabilistic model which declares a user as the winner when his dice number is greater than the same as the other player.",
+    image:
+      "https://user-images.githubusercontent.com/76873956/237994574-cf371612-4e4e-43fe-8fbc-581a1b75e453.png",
+    tags: ["HTML","CSS","JavaScript"],
+    category: "web app",
+    github: "https://github.com/ArijeetBanerjee/DICE-GAME",
+    webapp: "https://arijeetbanerjee.github.io/DICE-GAME/",
+  },
+  {
+    id: 6,
+    title: "TINDOG",
+    date: "Jun 2021",
+    description:
+      "Made a commercial webpage using HTML,CSS.",
+    image:
+      "https://user-images.githubusercontent.com/76873956/272167658-f8c27019-c9cd-4899-aa62-954c256e9c8d.png",
+    tags: ["HTML","CSS"],
+    category: "web app",
+    github: "https://github.com/ArijeetBanerjee/TINDOG",
+    webapp: "https://arijeetbanerjee.github.io/TINDOG/",
+  },
+  {
+    id: 7,
+    title: "Nike UI Design",
+    date: "Dec 2020 - Jan 2021",
+    description:
+      " Designed a sleek and visually appealing user interface (UI) for NIKE Lakers shoes using Figma. This project allowed me to explore the dynamic world of footwear design and translate it into a captivating digital experience. Through Figma, I incorporated elements that reflect the essence of NIKE Lakers shoes, focusing on aesthetics, usability, and brand identity.",
+    image:
+      "https://user-images.githubusercontent.com/76873956/271311569-7aaa8bae-48d8-42c0-ac8e-886e885933b5.png",
+    tags: ["Figma"],
+    category: "UI/UX",
+    github: "https://github.com/ArijeetBanerjee/UI-UX-Design",
+    webapp: "https://github.com/ArijeetBanerjee/UI-UX-Design",
+  },
+  {
+    id: 8,
+    title: "QR-Code-Generator",
+    date: "Jan 2021",
+    description:
+      "Developed a versatile QR code generator capable of converting any website link into a QR code. This project showcases my skills in web development and design. By seamlessly translating URLs into scannable QR codes, I aimed to simplify information sharing and enhance user convenience. This project not only strengthened my programming abilities but also highlighted the practical applications of QR codes in modern technology.",
+    image:
+      "https://user-images.githubusercontent.com/76873956/272177503-3625035c-7870-4103-971d-6ceb7d14c3f2.png",
+    tags: ["JavaScript","NodeJS","ExpressJS"],
+    category: "Backend",
+    github: "https://github.com/ArijeetBanerjee/QR-Code-Generator",
+    webapp: "https://github.com/ArijeetBanerjee/QR-Code-Generator",
+  },
+  {
+    id: 9,
+    title: "Website UI Design",
+    date: "Jan 2021",
+    description:
+      "Developed a Ecom Website Design UI using Figma.",
+    image:
+      "https://user-images.githubusercontent.com/76873956/271311789-af1761a3-223f-4067-b981-7f3ec2a709d6.png",
+    tags: ["Figma"],
+    category: "UI/UX",
+    github: "https://github.com/ArijeetBanerjee/UI-UX-Design",
+    webapp: "https://github.com/ArijeetBanerjee/UI-UX-Design",
+  },
+];
+
+
